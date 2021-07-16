e974a321a471a3d286418e5ade9502297fdc9150
yazantafesh <yazantafesh1@gmail.com>
2021-07-16 14:05:31 +0300

    fix route first try

--- src/Components/main/index.js
+++ src/Components/main/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '../home';
-import Admin from '../admin';
+import Admin from '../admin/index.js';
 import './home.css';
 
 function Main() {
